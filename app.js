1c0150e6091de222c4e38474161aa80bd7ed186c
cassandraGoose <cassandra.torske@gmail.com>
2023-07-11 11:10:54 -0600

    add api stuff for tour

--- app.js
+++ app.js
@@ -1,66 +1,38 @@
-// use a createError function to generate errors with appropriate properties easily
 var createError = require('http-errors');
-// gives you some features for web applications so you can build an api
 var express = require('express');
-// utilities for working with file and direcotry paths
 var path = require('path');
 
-// middleware that lets you deal with cookies
+// NEW: removed cookie parser because i'm not going to deal with cookies.
 var cookieParser = require('cookie-parser');
-// middleware that logs info about incoming http requests and responses
 var logger = require('morgan');
+// NEW: added helmet, which is a tool that sets HTTP response headers for us. 
+// this helps protect our apps from XSS and other attacks. It's not a silver bullet, though!
+const helmet = require('helmet');
+// NEW: added CORS. This way, we can make requests from different origins.
+const cors = require('cors');
 
-// part of the setup for using a modular routing system 
-// check out the files index.js and user.js to see them use express.Router()
-// express.Router() creates an instance of Router to allow for module route handlers. 
-// a route handler is the code that gets a request at a certain URL and does something with it. 
-// Let's take a look at what the two routes we have do. 
+// PRO tip. You'll probably need to have some kind of environment variable situation. 
+// Research .env and packages that may help you with that such as dotenv.
+
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// NEW: removed user routing system because I am not going to have users
 
-// instance of express
 var app = express();
 
-// view engine setup
-// if we have views, then this is the system we would use. 
-// I'm not going to cover a server that includes views, so we can ignore these. 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-// tell the express instance to use these tools. 
-// the logger we required earlier
 app.use(logger('dev'));
-// express.json() is built in middleware to parse incoming requests with JSON payloads.
 app.use(express.json());
-// express.urlencoded() is a built in middleware to parse incoming requests with URL-encoded payloads. ( a request that uses content-type: application/x-www-form-urlencoded or similar)
-app.use(express.urlencoded({ extended: false }));
-// the cookie parser we required earlier
-app.use(cookieParser());
-// built in middleware that serves static files using the path tool we required earlier
-app.use(express.static(path.join(__dirname, 'public')));
+// NEW: removed the static page serving because i'm going to be dealing with only JSON and not views. 
 
-// part of the setup for using a modular routing system 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
-// recall that a 404 message isn't _actually_ an error, so error handling won't catch it.
-// this little chunk of code catches those 404s and turns them into errors for us. neat!
 app.use(function(req, res, next) {
   next(createError(404));
 });
-// Note: app.use accepts a callback function that accepts various arguments. 
-// you'll use some of those arguments in some situations
-// and others in other situations. 
 
-// error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  // res.locals is a property that contains local variables scoped to the request.
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
+
+// PRO tip
+// some other funcitonality that may live in here is: 
+// validations. You want to make sure the data you're getting from the request is safe and valid.
+// queries or imported query functionality so you can access data form your Database!
+// POST, PUT, PATCH, DELETE routes.
+// You may also utilize the req, res, and next arguments.
+  // req: request object. use maybe to grab an id or check the body of the request
+  // res: response object. send json with this! 
+  // next: next function. executes the upcoming middleware. maybe use to trigger errors.
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  // res.render is for views! let's look at it, but we won't be digging into it. 
-  res.render('index', { title: 'Express' });
+  //NEW removed render functionality and instead am going to use res (an argument we have access to)
+  // to send JSON instead!
+  res.json({
+    message: 'Hello, world! Data would go here.'
+  })
 });
 
 module.exports = router;
